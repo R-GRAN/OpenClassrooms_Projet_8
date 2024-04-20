27ea88a44a969393113aad55bfdc141b904dbb44
Remy <remy.grangenois@gmail.com>
2024-04-21 00:31:47 +0200

    add gh-pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login/Login";
-import Error from "./pages/Error/Error";
 import "./assets/styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,8 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>

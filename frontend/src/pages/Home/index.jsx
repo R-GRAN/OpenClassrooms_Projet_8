ed31f54291474afd5c4cfd94eba5e1ff288242ee
Remy <remy.grangenois@gmail.com>
2024-04-20 15:07:36 +0200

    Presentation & Skills - jsx + scss

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import Banner from "../../components/Banner"
 import HeaderApp from "../../components/HeaderApp"
+import Presentation from "../../components/Presentation"
+import Skills from "../../components/Skills"
 
 function Home() {
   return (
     <>
 <HeaderApp/>
 <Banner/>
+<Presentation/>
+<Skills/>
     </>
   )
 }

bcba0d38130c24d78645c101e74d054c45cc4ae4
Remy <remy.grangenois@gmail.com>
2024-04-20 14:22:18 +0200

    Header & Banner -  jsx + sccs

--- frontend/src/components/HeaderApp.jsx
+++ frontend/src/components/HeaderApp.jsx
@@ -1,17 +1,28 @@
 function HeaderApp() {
-    return (
-        <header>
-        <span>Rémy Grangenois</span>
-        <nav>
-          <ul>
-            <li><a href="#presentation">Présentation</a></li>
-            <li><a href="#skills">Compétences</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    )
+  return (
+    <header className="header-style">
+      <span>Rémy Grangenois</span>
+      <button className="header-btn-sm-menu" aria-label="menu">
+        <i className="fa-solid fa-bars"></i>
+      </button>
+      <nav className="header-nav">
+        <ul className="header-menu">
+          <li>
+            <a href="#presentation">Présentation</a>
+          </li>
+          <li>
+            <a href="#skills">Compétences</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default HeaderApp
+export default HeaderApp;
